812b9d6cd33128c235996426367ea5b61f990a71
Bilal Sir <bilal.khalid.bsai-2024a@cecosian.edu.pk>
2026-06-02 02:40:47 +0500

    the new comit for first branch

--- my_code.py
+++ my_code.py
@@ -1,4 +1,4 @@
-students = [1,2,4,5,62,456,3,21,1,54,6]
-for x in students:
+main_people = [1,2,4,5,62,456,3,21,1,54,6]
+for x in main_people:
     print(x)
-    print('=' * 60 + 2)
+    print('=' * 60 + 'fdfdfdfdfdfdfdfd')
